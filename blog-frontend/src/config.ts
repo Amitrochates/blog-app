a31ebfc8c332727b4b262fca244e4880d9acd3fd
shlok agrawal <shlok.agrawal@hotmail.com>
2024-06-18 16:16:14 +0530

    backend url

--- blog-frontend/src/config.ts
+++ blog-frontend/src/config.ts
@@ -1,3 +1,2 @@
-// export const BACKEND_URL = "https://blog-backend.shlok-agrawal.workers.dev";
+export const BACKEND_URL = "https://blog-backend.shlok-agrawal.workers.dev";
 
-export const BACKEND_URL = "http://127.0.0.1:8787";
